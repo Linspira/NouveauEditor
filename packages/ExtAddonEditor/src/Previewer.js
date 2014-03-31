434f02c0e3e9c06c51d9e5b308ed772f30e0991c
Lee Boonstra <lee.boonstra@sencha.com>
2014-03-31 17:28:08 +0200

    working ext preview - touch doesn't work.

--- packages/ExtAddonEditor/src/Previewer.js
+++ packages/ExtAddonEditor/src/Previewer.js
@@ -6,34 +6,72 @@ Ext.define('ExtAddonEditor.Previewer', {
 	config: {
 		mode: null,
 		snippet: null,
+		urls: {
+			touch: {
+				cssUrls: [
+					'//extjs.cachefly.net/touch/sencha-touch-2.3.1/gpl/resources/css/sencha-touch.css'
+				],
+				libraryUrl: '//extjs.cachefly.net/touch/sencha-touch-2.3.1/gpl/sencha-touch-all-debug.js',
+			},
+			ext: {
+				cssUrls: ['//cdn.sencha.io/ext/gpl/4.2.0/resources/css/ext-all-neptune.css'],
+				libraryUrl: '//cdn.sencha.com/ext/gpl/4.2.0/ext-all-debug.js',
+			}
+		},
 	},
 
 	renderTpl: [
-		'<iframe height="100%" width="100%" frameBorder="0"></iframe>',
+		'<iframe height="100%" width="100%" frameBorder="0"><!DOCTYPE html></iframe>',
     ],
 
 	initializeHead: function(iframe) {
+		console.log(iframe);
+		iframe.head.innerHTML = "";
 
-		var css = document.createElement('link'),
-			script = document.createElement('script');
+		var me = this,
+			css = document.createElement('link'),
+			scriptFramework = document.createElement('script');
 
 		css.rel = 'stylesheet';
-		//css.href = url;
-		script.type = 'text/javascript';
-		//script.src = urls.libraryUrl;
-		script.innerHTML = this.getSnippet();
+		scriptFramework.type = 'text/javascript';	
+		css.href = me.urls.touch.cssUrls[0];
+		scriptFramework.src = me.urls.touch.libraryUrl;
 
-		//iframe.head.appendChild(css);
-		iframe.head.appendChild(script);
+		iframe.write('<!doctype HTML>\n' + iframe.head.outerHTML + iframe.body.outerHTML);
+		iframe.head.appendChild(css);
+		iframe.head.appendChild(scriptFramework);
 	},
+	appendMyCode: function(iframe){
+		var me = this,
+			script = iframe.createElement('script');
+		
+		iframe.body.innerHTML = "";
+		script.type = 'text/javascript';
+		
+		//script.innerHTML = "console.log(Ext); Ext.onReady(function() { " +
+		//	this.getSnippet() + 
+		//"});";
+		//script.innerHTML = this.getSnippet();
 
-    show: function () {
-        var me = this;
+		script.innerHTML = "Ext.application({ name : 'Editor', launch : function() { console.dir(Ext.Viewport); " +
+			this.getSnippet() +
+    	"}});"
+
+		iframe.body.appendChild(script);
+	},
+    onRender: function () {
+        var me = this,
+        	iframe = me.el.dom.lastChild.contentDocument;
+       
         me.callParent();
-
-        console.log(me.getSnippet(), me.getMode());
-        var iframe = me.el.dom.lastChild.contentDocument;
         me.initializeHead(iframe);
+    },
+    show: function(){
+		var me = this,
+			iframe = me.el.dom.lastChild.contentDocument; 
+	   me.callParent();
 
+       console.log(iframe);
+       me.appendMyCode(iframe); 	
     }
 });
